584ade198a78a1bf942d5ff93e267c9f3e632ada
alper <32721462+alperyasar@users.noreply.github.com>
2020-08-18 22:15:31 +0300

    update
    new update for draw a graph

--- Assets/Scripts/RotationOfBox.cs
+++ Assets/Scripts/RotationOfBox.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,8 @@ public class RotationOfBox : MonoBehaviour
 {
     //  public Transform upperArm1;
     // Start is called before the first frame update
-    public int purpose = 1;
+    public int purpose = 2;
+    public float xAxis, zAxis;
     int upDown = 1, i = 0;
     int turnBack1, turnBack2;
     float temp1, temp3;
@@ -19,7 +21,9 @@ public class RotationOfBox : MonoBehaviour
         position2 = GameObject.FindGameObjectWithTag("upperArm2").transform.position;
         position3 = GameObject.FindGameObjectWithTag("upperArm3").transform.position;
         temp3 = position3.y;
-        position4 = GameObject.FindGameObjectWithTag("upperArm4").transform.position;
+        ball = GameObject.FindGameObjectWithTag("ball").transform.position;
+        xAxis = ball.x;
+        zAxis = ball.z;
     }
 
     // Update is called once per frame
@@ -49,11 +53,100 @@ public class RotationOfBox : MonoBehaviour
         
         else if (purpose == 2)
         {
-            if (position1.y != temp1)
-                rotateArm1();
-            if (position3.y != temp3)
-                rotateArm3();
+
+            if (ball.x == xAxis && ball.z == zAxis)
+            {
+                rotateOldPurpose21();
+                rotateOldPurpose22();
+            }
+            else if (ball.x > xAxis && ball.z > zAxis)
+            {
+                transform.localRotation = Quaternion.Euler(-95, 35, 145);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 17.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 21.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 21.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 17.37371f, position4.z);
+            }
+            else if (ball.x < xAxis && ball.z > zAxis)
+            {
+                transform.localRotation = Quaternion.Euler(-84, 138, 40);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 17.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 21.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 17.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 21.37371f, position4.z);
+            }
+            else if (ball.x > xAxis && ball.z < zAxis)
+            {
+                transform.localRotation = Quaternion.Euler(-96, 135, 44);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 21.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 17.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 21.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 17.37371f, position4.z);
+            }
+            else if (ball.x < xAxis && ball.z < zAxis)
+            {
+                transform.localRotation = Quaternion.Euler(-96, 245, -68);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 21.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 17.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 17.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 21.37371f, position4.z);
+            }
+            else if (ball.x > xAxis )
+            {
+                transform.localRotation = Quaternion.Euler(-95, 90, 90);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 21.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 17.37371f, position4.z);
+            }
+            else if (ball.x < xAxis)
+            {
+                transform.localRotation = Quaternion.Euler(-85, 90, 90);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 29.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 17.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 21.37371f, position4.z);
+            }
+            else if (ball.z < zAxis)
+            {
+                transform.localRotation = Quaternion.Euler(-95, 180, 0);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 21.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 17.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f, position4.z);
+            }
+            else if (ball.z > zAxis)
+            {
+                transform.localRotation = Quaternion.Euler(-85, 180, 0);
+                GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 17.37371f, position1.z);
+                GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 21.37371f, position2.z);
+                GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f, position3.z);
+                GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f, position4.z);
+            }
+            /*
+            if ((int)ball.x != (int)xAxis)
+            {
+                transform.localRotation = Quaternion.Euler(transform.eulerAngles.x, 0, 180);
+                rotateWithJumpPurpose22();
+            }
+            if ((int)ball.z != (int)zAxis)
+            {
+                transform.localRotation = Quaternion.Euler(transform.eulerAngles.x, 90, 90);
+                rotateWithJumpPurpose21();
+
+            }
+            if((int)ball.x == (int)xAxis && (int)ball.z == (int)zAxis)
+            {
+                rotateOldPurpose21();
+                rotateOldPurpose22();
+            }
+            if (transform.eulerAngles.x > 280)
+                transform.localEulerAngles = new Vector3(280, transform.eulerAngles.y, transform.eulerAngles.z);
+            else if (transform.eulerAngles.x < 260)
+                transform.localEulerAngles = new Vector3(260, transform.eulerAngles.y, transform.eulerAngles.z);*/
+
         }
+
 
 
 
@@ -86,14 +179,14 @@ public class RotationOfBox : MonoBehaviour
         if (ball.z > -110.1277f)
         {
             turnBack1 = 1;
-            transform.Rotate(Vector3.right, 2);
+            transform.Rotate(Vector3.right, 80 * Time.deltaTime);
             GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, position1.y + 2, position1.z);
             GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, position2.y -2, position2.z);
         }
         else if (ball.z < -110.1277f)
         {
             turnBack1 = 2;
-            transform.Rotate(Vector3.left, 2);
+            transform.Rotate(Vector3.left, 80 * Time.deltaTime);
             GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, position1.y -2, position1.z);
             GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, position2.y + 2, position2.z);
         }
@@ -103,14 +196,14 @@ public class RotationOfBox : MonoBehaviour
         if (ball.x > 25.38906f)
         {
             turnBack2 = 1;
-            transform.Rotate(Vector3.down, -2);
+            transform.Rotate(Vector3.down, -80 * Time.deltaTime);
             GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, position3.y + 2, position3.z);
             GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, position4.y -2 , position4.z);
         }
         else if (ball.x < 25.38906f)
         {
             turnBack2 = 2;
-            transform.Rotate(Vector3.up, -2);
+            transform.Rotate(Vector3.up, -80 * Time.deltaTime);
             GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, position3.y -2, position3.z);
             GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, position4.y +2, position4.z);
         }
@@ -194,5 +287,135 @@ public class RotationOfBox : MonoBehaviour
         position4 = GameObject.FindGameObjectWithTag("upperArm4").transform.position;
         temp3 = position3.y;
 
+    }
+
+    void rotateWithJumpPurpose21()
+    {
+        if (ball.z > zAxis)
+        {
+            turnBack1 = 1;
+               transform.Rotate(Vector3.left, 80 * Time.deltaTime);
+            GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f + 80 * Time.deltaTime, position1.z);
+            GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f - 80 * Time.deltaTime, position2.z);
+            /*   if(ball.z > zAxis + 5)
+               {
+                   Debug.Log(ball.z + " sikik1 " + zAxis);
+
+                   transform.localRotation = Quaternion.Euler(100, 0, 180);
+                   GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f + (float)(Math.Cos(10) * 10) , position1.z);
+                   GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f - (float)(Math.Cos(10) * 10), position2.z);
+
+               }
+               else
+               {
+                   Debug.Log(ball.z + " sikik2 " + zAxis);
+                   transform.localRotation = Quaternion.Euler(90 + ((ball.z-zAxis) * 2), 0, 180);
+                   GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f + (float)(Math.Cos(10) * ((ball.z - zAxis) * 2)), position1.z);
+                   GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f - (float)(Math.Cos(10) * ((ball.z - zAxis) * 2)), position2.z);
+
+               }*/
+
+        }
+        else if (ball.z < zAxis)
+        {
+            turnBack1 = 2;
+                transform.Rotate(Vector3.right, 80 * Time.deltaTime);
+            GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f - 80 * Time.deltaTime, position1.z);
+            GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f + 80 * Time.deltaTime, position2.z);
+            /*  if (ball.z < zAxis - 5)
+              {
+                  transform.localRotation = Quaternion.Euler(80, 0, 180);
+                  GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f - (float)(Math.Cos(10) * 10), position1.z);
+                  GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f + (float)(Math.Cos(10) * 10), position2.z);
+
+              }
+              else
+              {
+                  transform.localRotation = Quaternion.Euler(90 - ((zAxis - ball.z) * 2), 0, 180);
+                  GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f - (float)(Math.Cos(10) * ((zAxis - ball.z) * 2)), position1.z);
+                  GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f + (float)(Math.Cos(10) * ((zAxis - ball.z) * 2)), position2.z);
+
+              }*/
+        }
+    }
+    void rotateWithJumpPurpose22()
+    {
+        if (ball.x > xAxis)
+        {
+            turnBack2 = 1;
+
+           transform.Rotate(Vector3.up, 80 * Time.deltaTime);
+            GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f - 80 * Time.deltaTime, position3.z);
+            GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f + 80 * Time.deltaTime, position4.z);
+            //    transform.Rotate(80 * Time.deltaTime, 90, 90);
+            /*   if (ball.x > xAxis + 5)
+               {
+                   transform.localRotation = Quaternion.Euler(-100, 90, 90);
+                   GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f - (float)(Math.Cos(10) * 10), position3.z);
+                   GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f + (float)(Math.Cos(10) * 10), position4.z);
+
+               }
+               else
+               {
+                   transform.localRotation = Quaternion.Euler(-90 - ((ball.x - xAxis) * 2), 90, 90);
+                   GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f - (float)(Math.Cos(10) * ((ball.x - xAxis) * 2)), position3.z);
+                   GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f + (float)(Math.Cos(10) * ((ball.x - xAxis) * 2)), position4.z);
+
+               }*/
+        }
+        else if (ball.x < xAxis)
+        {
+            turnBack2 = 2;
+               transform.Rotate(Vector3.down, 80 * Time.deltaTime);
+            GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f + 80*Time.deltaTime, position3.z);
+            GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f - 80 * Time.deltaTime, position4.z);
+            /*   //    transform.Rotate(80 * Time.deltaTime, 90, 90);
+               if (ball.z < zAxis - 5)
+               {
+                   transform.localRotation = Quaternion.Euler(-80, 90, 90);
+                   GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f + (float)(Math.Cos(10) * 10), position3.z);
+                   GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f - (float)(Math.Cos(10) * 10), position4.z);
+
+               }
+               else
+               {
+                   transform.localRotation = Quaternion.Euler(-90 + ((xAxis - ball.x) * 2), 90, 90);
+                   GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f + (float)(Math.Cos(10) * ((xAxis - ball.x) * 2)), position3.z);
+                   GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f - (float)(Math.Cos(10) * ((xAxis - ball.x) * 2)), position4.z);
+
+               }*/
+        }
+    }
+    void rotateOldPurpose21()
+    {
+        if (turnBack1 == 1)
+        {
+            transform.localEulerAngles = new Vector3(-90, 90, 90);
+            GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f, position1.z);
+            GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f, position2.z);
+        }
+        else if (turnBack1 == 2)
+        {
+            transform.localEulerAngles = new Vector3(-90, 90, 90);
+            GameObject.FindGameObjectWithTag("upperArm1").transform.position = new Vector3(position1.x, 19.37371f, position1.z);
+            GameObject.FindGameObjectWithTag("upperArm2").transform.position = new Vector3(position2.x, 19.37371f, position2.z);
+        }
+        turnBack1 = 0;
+    }
+    void rotateOldPurpose22()
+    {
+        if (turnBack2 == 1)
+        {
+            transform.localEulerAngles = new Vector3(-90, 90, 90);
+            GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f, position3.z);
+            GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f, position4.z);
+        }
+        else if (turnBack2 == 2)
+        {
+            transform.localEulerAngles = new Vector3(-90, 90, 90);
+            GameObject.FindGameObjectWithTag("upperArm3").transform.position = new Vector3(position3.x, 19.37371f, position3.z);
+            GameObject.FindGameObjectWithTag("upperArm4").transform.position = new Vector3(position4.x, 19.37371f, position4.z);
+        }
+        turnBack2 = 0;
     }
 }
---
+++ Assets/ba.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 80 * Time.deltaTime);
+        transform.Rotate(Vector3.right, 80 * Time.deltaTime);
+    }
+}
